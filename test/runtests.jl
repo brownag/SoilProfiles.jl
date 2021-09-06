0a5668328fda6314a31eb733bb9cc6ecab0953b3
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-09-06 11:31:41 -0700

    v0.2.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,19 @@ s = DataFrame(pid = 1:6, elev = 100:105)
 l = DataFrame(pid = [1,1,1,1,1,2,2,2,2,2,3,3,3,3,3],
               top = [0,10,20,30,40,0,5,10,15,20,0,20,40,60,80],
               bot = [10,20,30,40,50,5,10,15,20,25,20,40,60,80,100])
-spc = SoilProfile("pid", s, l)
+s3 = DataFrame(pid = [7, 2, 3, 4, 5, 6], elev = 100:105)
+l2 = DataFrame(pid = [7,7,7,7,7,2,2,2,2,2,3,3,3,3,3],
+               foo = [0,10,20,30,40,0,5,10,15,20,0,20,40,60,80],
+               bar = [10,20,30,40,50,5,10,15,20,25,20,40,60,80,100])
+spc = SoilProfile("pid", ["top", "bot"], s, l)
+spc2 = SoilProfile("pid", ["foo", "bar"], s, l2)
+spc3 = SoilProfile("pid", ["foo", "bar"], s3, l2)
 
 @testset "Constructors" begin
     @test length(spc) == 6
     @test isValid(spc) == true
+    @test isValid(spc2) == false
+    @test isValid(spc3) == true
     @test isempty(SoilProfile()) == true
     @test isValid(SoilProfile()) == true
     show(spc)
@@ -25,6 +33,10 @@ end
 
 @testset "Integrity" begin
     @test sitesWithoutLayers(spc) == [4,5,6]
+    @test checkIntegrity(spc)
+    @test all(checkTopology(spc)[:,:check])
+    @test checkIntegrity(spc2) == false
+    @test site(reorder(spc3))[:,pidname(spc3)] == 2:7
 end
 
 @testset "Iteration" begin
